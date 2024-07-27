82dc35e949f6ac9f2cab6436038e3b81c98f7a65
Marty Zalega <marty@zalega.me>
2024-07-28 08:24:36 +1000

    Remove helpers

--- src/safaribookmarks/__init__.py
+++ src/safaribookmarks/__init__.py
@@ -1,6 +1,5 @@
 from .safaribookmarks import SafariBookmarks, SafariBookmarkItem
-from .helpers import load, dump
 
 open = SafariBookmarks.open
 
-__all__ = ["load", "dump", "SafariBookmarks", "SafariBookmarkItem", "open"]
+__all__ = ["SafariBookmarks", "SafariBookmarkItem", "open"]
