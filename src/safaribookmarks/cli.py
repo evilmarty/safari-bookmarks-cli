9a0df94d8cf0a170425d1a1c884c02fe16776afd
Marty Zalega <marty@zalega.me>
2024-07-30 15:52:26 +1000

    Refactor CLI

--- src/safaribookmarks/cli.py
+++ src/safaribookmarks/cli.py
@@ -7,8 +7,12 @@ DEFAULT_LIST_FORMAT = "{prefix: <{depth}}{title: <50}{type: <6}{id: <38}{url}"
 
 class CLI:
     def __init__(self, path: str, out: IO) -> None:
-        self.path = path
+        self.bookmarks = SafariBookmarks.open(path)
         self.output = out
+
+    @property
+    def path(self) -> str:
+        return str(self.bookmarks.path)
 
     def run(self, command: str, **kwargs) -> None:
         if command is None:
@@ -18,23 +22,15 @@ class CLI:
             raise ValueError(f"Invalid command: {command}")
         func(**kwargs)
 
-    @contextmanager
-    def _with_bookmarks(
-        self, update: bool = False
-    ) -> Generator[SafariBookmarks, None, None]:
-        bookmarks = SafariBookmarks.open(self.path)
-        yield bookmarks
-        if update:
-            bookmarks.save()
-
-    def _get_or_walk(
-        self, path: List[str], bookmarks: SafariBookmarks
-    ) -> Optional[SafariBookmarkItem]:
+    def _get_or_walk(self, path: List[str]) -> Optional[SafariBookmarkItem]:
         if len(path) == 1:
-            result = bookmarks.get(path[0])
+            result = self.bookmarks.get(path[0])
             if result is not None:
                 return result
-        return bookmarks.walk(*path)
+        return self.bookmarks.walk(*path)
+
+    def _save(self) -> None:
+        self.bookmarks.save()
 
     def _render_item(self, item: SafariBookmarkItem, format: str, depth: int = 0):
         self.output.write(
@@ -74,11 +70,10 @@ class CLI:
                 self._render_item(root, format=format)
 
     def list(self, path: List[str] = [], **kwargs):
-        with self._with_bookmarks() as root:
-            target = self._get_or_walk(path, root)
-            if target is None:
-                raise ValueError("Target not found")
-            self._render(target, only_children=target.is_folder, **kwargs)
+        target = self._get_or_walk(path)
+        if target is None:
+            raise ValueError("Target not found")
+        self._render(target, only_children=target.is_folder, **kwargs)
 
     def add(
         self,
@@ -87,73 +82,66 @@ class CLI:
         url: Optional[str] = None,
         path: List[str] = [],
         list=False,
-        **kwargs,
     ):
-        with self._with_bookmarks(True) as root:
-            target = self._get_or_walk(path, root)
-            if target is None or not target.is_folder:
-                raise ValueError("Invalid destination")
-            if list:
-                if url:
-                    raise ValueError("URL is not supported by lists")
-                if not title:
-                    raise ValueError("Title is required")
-                item = target.add_folder(title=title, id=uuid)
-            elif url is None:
-                raise ValueError("URL is required")
-            else:
-                item = target.add_bookmark(url=url, id=uuid, title=title)
-            self._render(item, **kwargs)
+        target = self._get_or_walk(path)
+        if target is None or not target.is_folder:
+            raise ValueError("Invalid destination")
+        if list:
+            if url:
+                raise ValueError("URL is not supported by lists")
+            if not title:
+                raise ValueError("Title is required")
+            target.add_folder(title=title, id=uuid)
+        elif url is None:
+            raise ValueError("URL is required")
+        else:
+            target.add_bookmark(url=url, id=uuid, title=title)
+        self._save()
 
-    def remove(self, path: List[str], **kwargs):
-        with self._with_bookmarks(True) as root:
-            target = self._get_or_walk(path, root)
-            if target is None:
-                raise ValueError("Target not found")
-            if parent := target.parent:
-                parent.remove(target)
-            self._render(root, **kwargs)
+    def remove(self, path: List[str]):
+        target = self._get_or_walk(path)
+        if target is None:
+            raise ValueError("Target not found")
+        if parent := target.parent:
+            parent.remove(target)
+        self._save()
 
-    def move(self, path: List[str], to: List[str] = [], **kwargs):
-        with self._with_bookmarks(True) as root:
-            target = self._get_or_walk(path, root)
-            if target is None:
-                raise ValueError("Target not found")
-            if not to:
-                raise ValueError("Missing destination")
-            dest = self._get_or_walk(to, root)
-            if dest is None or not dest.is_folder:
-                raise ValueError("Invalid destination")
-            dest.append(target)
-            self._render(dest, **kwargs)
+    def move(self, path: List[str], to: List[str] = []):
+        target = self._get_or_walk(path)
+        if target is None:
+            raise ValueError("Target not found")
+        if not to:
+            raise ValueError("Missing destination")
+        dest = self._get_or_walk(to)
+        if dest is None or not dest.is_folder:
+            raise ValueError("Invalid destination")
+        dest.append(target)
+        self._save()
 
     def edit(
         self,
         path: List[str],
         title: Optional[str] = None,
         url: Optional[str] = None,
-        **kwargs,
     ):
-        with self._with_bookmarks(True) as root:
-            target = self._get_or_walk(path, root)
-            if target is None:
-                raise ValueError("Target not found")
-            if title is not None:
-                target.title = title
-            if url is not None:
+        target = self._get_or_walk(path)
+        if target is None:
+            raise ValueError("Target not found")
+        if title is not None:
+            target.title = title
+        if url is not None:
+            target.url = url
+            if target.is_bookmark:
                 target.url = url
-                if target.is_bookmark:
-                    target.url = url
-                else:
-                    raise ValueError("Cannot update target url")
-            self._render(target, **kwargs)
+            else:
+                raise ValueError("Cannot update target url")
+        self._save()
 
-    def empty(self, path: List[str], **kwargs):
-        with self._with_bookmarks(True) as root:
-            target = self._get_or_walk(path, root)
-            if target is None:
-                raise ValueError("Target not found")
-            if not target.is_folder:
-                raise ValueError("Target is not a list")
-            target.empty()
-            self._render(root, only_children=target.is_folder, **kwargs)
+    def empty(self, path: List[str]):
+        target = self._get_or_walk(path)
+        if target is None:
+            raise ValueError("Target not found")
+        if not target.is_folder:
+            raise ValueError("Target is not a list")
+        target.empty()
+        self._save()
